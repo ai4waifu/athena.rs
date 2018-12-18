b3835e6ac017ba5bfec053468267f0ffda35233b
aster <aster@vers.site>
2018-12-18 12:32:44 +0000

    ✨ Expose `FieldExtension` tower and Galois APIs on `athena` facade
    Re-export `FieldExtension`, `GroupTable`, `execute_galois_with_tables`, Frobenius helpers, and related field automorphisms from `athena`.

--- projects/athena-engine/src/galois/mod.rs
+++ projects/athena-engine/src/galois/mod.rs
@@ -1,6 +1,8 @@
 //! 伽罗瓦理论 — 扩张性质、自同构、伽罗瓦群。
 //!
-//! 已实现：请求/值对象合同。完整 Galois 群计算分派待扩展。
+//! 已实现：`$\mathbb{F}_{p^n}/\mathbb{F}_p$` 上可分/正规/`IsGalois`、Frobenius 自同构，
+//! 以及经 [`execute_galois_with_tables`] 的完整伽罗瓦群（循环 `$C_n$`）。
+//! 多项式入口 / 固定域仍 `Unevaluated`。
 //!
 //! 调用 `polynomial` / `field`，不反向拥有其语义，也不复制实现。
 
