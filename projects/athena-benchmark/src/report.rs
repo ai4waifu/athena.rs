26744903cbf7d1d5f6f57f1b4ba1c87fdfbebc87
aster <aster@vers.site>
2019-02-08 14:58:17 +0000

    ✨ Add kernel arena e2e GcMode fields to bench reports
    Introduce `ReportTier` with suggested `GcMode`, force peak arena/scratch and gc time into fixture JSON, and expose `--tier` on `athena-bench`.

--- projects/athena-benchmark/src/report.rs
+++ projects/athena-benchmark/src/report.rs
@@ -4,6 +4,38 @@ use serde::Serialize;
 
 use crate::{env::BenchEnv, validate::ValidationSummary};
 
+/// Living `15`/`12` 三类报告分层（禁止互相冒充）。
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ReportTier {
+    /// Limb / machine kernel：建议 `GcMode::Disabled`。
+    Kernel,
+    /// Arena / 分配：建议 `GcMode::Deferred`。
+    Arena,
+    /// 端到端 CAS：建议 `GcMode::Auto`。
+    EndToEnd,
+}
+
+impl ReportTier {
+    /// 稳定短名。
+    pub fn as_str(self) -> &'static str {
+        match self {
+            Self::Kernel => "kernel",
+            Self::Arena => "arena",
+            Self::EndToEnd => "end_to_end",
+        }
+    }
+
+    /// 对应建议的 `GcMode` 名（报告字段，非强制切换）。
+    pub fn suggested_gc_mode(self) -> &'static str {
+        match self {
+            Self::Kernel => "disabled",
+            Self::Arena => "deferred",
+            Self::EndToEnd => "auto",
+        }
+    }
+}
+
 /// 完整报告（一次 `athena-bench` 运行）。
 #[derive(Debug, Clone, Serialize)]
 pub struct Report {
@@ -38,6 +70,16 @@ pub struct FixtureReport {
     pub alloc_bytes: Option<u64>,
     /// 峰值 RSS（尽力而为，常为 `null`）。
     pub peak_rss_bytes: Option<u64>,
+    /// 报告分层（kernel / arena / end_to_end）。
+    pub report_tier: Option<ReportTier>,
+    /// 本次采样有效的 `GcMode` 名。
+    pub gc_mode: Option<String>,
+    /// 峰值 arena resident 字节。
+    pub peak_arena_bytes: Option<u64>,
+    /// 峰值 scratch 字节。
+    pub peak_scratch_bytes: Option<u64>,
+    /// 累计 GC 时间（纳秒）。
+    pub gc_time_ns: Option<u64>,
     /// 校验摘要。
     pub validation: ValidationSummary,
     /// 跳过或回退原因。
