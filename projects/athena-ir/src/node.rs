1aea97e7e49cb362a846ba9fd2a67e9a349f091a
aster <aster@vers.site>
2019-04-09 01:53:38 +0000

    ♻️ Teach IR terms `try_clone_in` and avoid atom Clone in rewriter
    Drop `Clone` on `AtomKind` and `TermKind` now that `NumericValue` is non-Clone.
    Add `try_clone_in` for owning payload copies under `NumericContext`.
    Rewrite `fold_constants` to copy only `TermId` children instead of cloning whole `TermKind` atoms.

--- projects/athena-ir/src/node.rs
+++ projects/athena-ir/src/node.rs
@@ -1,10 +1,12 @@
 //! Core CAS IR term 种类（arena 持有，由 [`TermId`](athena_types::TermId) 引用）。
 
-use athena_numeric::NumericValue;
-use athena_types::{OperatorId, SourceSpan, SymbolId, TermId};
+use athena_numeric::{NumericContext, NumericValue};
+use athena_types::{OperatorId, Result, SourceSpan, SymbolId, TermId};
 
 /// 原子 term 载荷。
-#[derive(Debug, Clone, PartialEq)]
+///
+/// Living `19`：不实现 [`Clone`]（[`NumericValue`] 无 `Clone`）。深复制用 [`Self::try_clone_in`]。
+#[derive(Debug, PartialEq)]
 pub enum AtomKind {
     /// 内核数字（唯一数值真相源：[`NumericValue`]）。
     Number(NumericValue),
@@ -14,8 +16,22 @@ pub enum AtomKind {
     Symbol(SymbolId),
 }
 
+impl AtomKind {
+    /// Owning 复制：数字经 [`NumericValue::try_clone_in`]。
+    pub fn try_clone_in(&self, ctx: &NumericContext) -> Result<Self> {
+        Ok(match self {
+            Self::Number(n) => Self::Number(n.try_clone_in(ctx)?),
+            Self::String(s) => Self::String(s.clone()),
+            Self::Symbol(id) => Self::Symbol(*id),
+        })
+    }
+
+}
+
 /// Core IR 中的 term 节点。
-#[derive(Debug, Clone, PartialEq)]
+///
+/// Living `19`：不实现 [`Clone`]。节点经 arena `TermId` 引用；载荷复制用 [`Self::try_clone_in`]。
+#[derive(Debug, PartialEq)]
 pub enum TermKind {
     /// 原子值。
     Atom(AtomKind),
@@ -31,6 +47,16 @@ pub enum TermKind {
 }
 
 impl TermKind {
+    /// Owning 复制。
+    pub fn try_clone_in(&self, ctx: &NumericContext) -> Result<Self> {
+        Ok(match self {
+            Self::Atom(a) => Self::Atom(a.try_clone_in(ctx)?),
+            Self::List(xs) => Self::List(xs.clone()),
+            Self::App { op, args } => Self::App { op: *op, args: args.clone() },
+        })
+    }
+
+
     /// 是否为原子 term。
     pub fn is_atom(&self) -> bool {
         matches!(self, Self::Atom(_))
