d1cc06ddce19463a20a5a184f4de05877f875ea8
aster <aster@vers.site>
2019-06-28 16:43:08 +0000

    ✨ Add `ExecutionCompiler` and `ReferenceExecutor`
    Bootstrap atom-term lowering to LoadTerm/Return, SSA reference execution without an operand stack, and unit tests for atom replay plus typed Boolean branch.

--- projects/athena-engine/src/execution/compiler/mod.rs
+++ projects/athena-engine/src/execution/compiler/mod.rs
@@ -1,12 +1,16 @@
-//! `ExecutionCompiler` — `AthenaRequest` + Session snapshot → [`ExecutionModule`](crate::execution::ir::ExecutionModule).
+//! `ExecutionCompiler` — `AthenaRequest` + Session snapshot → [`ExecutionModule`].
 //!
-//! Contract freeze only: no lowering implementation and no bridge to the old
+//! Bootstrap lowering covers immutable atom terms only. No bridge to the old
 //! stack VM / `KernelIR` path.
 
-use athena_types::{Diagnostic, DiagnosticCode, Result};
+use athena_ir::TermNode;
+use athena_types::{Diagnostic, DiagnosticCode, Result, TermId};
 
 use crate::api::request::AthenaRequest;
-use crate::execution::ir::ExecutionModule;
+use crate::execution::ir::{
+    BasicBlock, BlockId, CapturedRoot, CapturedRootId, ExecutionModule, ExecutionValueType, ModuleFingerprint, Operation,
+    OperationKind, Region, RegionId, SsaValueId, verify_module,
+};
 use crate::runtime::session::Session;
 
 /// Compiles one request into a verified [`ExecutionModule`].
@@ -20,12 +24,89 @@ impl ExecutionCompiler {
     }
 
     /// Lower a request against a Session snapshot into `ExecutionIR`.
-    ///
-    /// Freeze status: returns [`DiagnosticCode::UnsupportedOperation`] until the
-    /// cutover wires real lowering. Must not call the old VM.
-    pub fn compile(&self, _session: &Session, _request: &AthenaRequest) -> Result<ExecutionModule> {
-        Err(Diagnostic::new(DiagnosticCode::UnsupportedOperation)
-            .detail("component", "ExecutionCompiler")
-            .detail("status", "contract_frozen_not_wired"))
+    pub fn compile(&self, session: &Session, request: &AthenaRequest) -> Result<ExecutionModule> {
+        match request {
+            AthenaRequest::Term(term) => self.compile_term(session, *term),
+            AthenaRequest::Command(_) | AthenaRequest::Control(_) | AthenaRequest::Goal(_) => {
+                Err(Diagnostic::new(DiagnosticCode::UnsupportedOperation)
+                    .detail("component", "ExecutionCompiler")
+                    .detail("status", "request_kind_not_lowered")
+                    .detail("kind", request.kind_name()))
+            }
+        }
+    }
+
+    fn compile_term(&self, session: &Session, term: TermId) -> Result<ExecutionModule> {
+        let Some(node) = session.arena.get(term) else {
+            return Err(Diagnostic::new(DiagnosticCode::InvalidIndex)
+                .detail("component", "ExecutionCompiler")
+                .detail("reason", "missing_term"));
+        };
+        match node {
+            TermNode::Atom(_) => self.compile_atom_term(term),
+            TermNode::List(_) | TermNode::Application { .. } => Err(Diagnostic::new(DiagnosticCode::UnsupportedOperation)
+                .detail("component", "ExecutionCompiler")
+                .detail("status", "compound_term_not_lowered")),
+        }
+    }
+
+    fn compile_atom_term(&self, term: TermId) -> Result<ExecutionModule> {
+        let root_id = CapturedRootId(0);
+        let value = SsaValueId(0);
+        let block = BasicBlock {
+            id: BlockId(0),
+            parameters: Vec::new(),
+            operations: vec![Operation {
+                result: Some(value),
+                result_type: ExecutionValueType::Term,
+                kind: OperationKind::LoadTerm { root: root_id },
+                effect_in: None,
+                effect_out: None,
+            }],
+            terminator: crate::execution::ir::Terminator::return_value(value),
+        };
+        let region = Region::from_entry_block(RegionId(0), block, vec![ExecutionValueType::Term]);
+        let mut module = ExecutionModule {
+            inputs: Vec::new(),
+            constants: Vec::new(),
+            captured_roots: vec![CapturedRoot::term(term)],
+            regions: vec![region],
+            effect_edges: Vec::new(),
+            exits: Vec::new(),
+            provider_calls: Vec::new(),
+            fingerprint: ModuleFingerprint(0),
+        };
+        module.fingerprint = ModuleFingerprint::of_module(&module);
+        verify_module(&module)?;
+        Ok(module)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::runtime::session::Session;
+
+    #[test]
+    fn compile_atom_term_module() {
+        let mut session = Session::new();
+        let term = session.builder().int(3, Default::default());
+        let module = ExecutionCompiler::new()
+            .compile(&session, &AthenaRequest::Term(term))
+            .expect("atom");
+        assert_eq!(module.captured_roots, vec![CapturedRoot::term(term)]);
+        assert_eq!(module.regions.len(), 1);
+    }
+
+    #[test]
+    fn compile_application_rejected() {
+        let mut session = Session::new();
+        let x = session.builder().symbol("x", Default::default());
+        let plus = session.operators.intern("Plus");
+        let term = session.builder().application(plus, vec![x, x], Default::default());
+        let err = ExecutionCompiler::new()
+            .compile(&session, &AthenaRequest::Term(term))
+            .expect_err("compound");
+        assert_eq!(err.code, DiagnosticCode::UnsupportedOperation);
     }
 }
--- projects/athena-engine/src/execution/reference/mod.rs
+++ projects/athena-engine/src/execution/reference/mod.rs
@@ -1,15 +1,31 @@
-//! `ReferenceExecutor` — correctness / replay backend for [`ExecutionModule`](crate::execution::ir::ExecutionModule).
+//! `ReferenceExecutor` — correctness / replay backend for [`ExecutionModule`].
 //!
 //! Executes SSA blocks without an operand stack. Not a wrapper around the old VM.
 
-use athena_types::{Diagnostic, DiagnosticCode, Result, ResultId};
+use std::collections::HashMap;
 
-use crate::execution::ir::ExecutionModule;
+use athena_types::{ComputationStatus, Diagnostic, DiagnosticCode, Result, ResultId, TermId};
+
+use crate::execution::ir::{
+    BlockId, CapturedRoot, ConstantValue, ExecutionModule, OperationKind, RegionId, SsaValueId, Terminator, verify_module,
+};
+use crate::runtime::results::{ComputationResult, CoverageStatus, ResultProvenance};
 use crate::runtime::session::Session;
 
 /// Semantic oracle backend shared by parity tests and deterministic replay.
 #[derive(Debug, Default)]
 pub struct ReferenceExecutor {}
+
+/// SSA runtime slot (session-local; not an identity domain shared with `TermId`).
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Slot {
+    /// Term handle from `TermStore`.
+    Term(TermId),
+    /// Typed Boolean.
+    Boolean(bool),
+    /// Unit.
+    Unit,
+}
 
 impl ReferenceExecutor {
     /// Create a reference executor.
@@ -18,11 +34,240 @@ impl ReferenceExecutor {
     }
 
     /// Execute a verified module in the given Session / runtime context.
-    ///
-    /// Freeze status: rejects until cutover implements block-wise SSA evaluation.
-    pub fn execute(&self, _session: &mut Session, _module: &ExecutionModule) -> Result<ResultId> {
-        Err(Diagnostic::new(DiagnosticCode::UnsupportedOperation)
-            .detail("component", "ReferenceExecutor")
-            .detail("status", "contract_frozen_not_wired"))
+    pub fn execute(&self, session: &mut Session, module: &ExecutionModule) -> Result<ResultId> {
+        verify_module(module)?;
+        let region_id = module.entry_region().ok_or_else(|| {
+            Diagnostic::new(DiagnosticCode::UnsupportedOperation)
+                .detail("component", "ReferenceExecutor")
+                .detail("reason", "missing_entry_region")
+        })?;
+        let returned = self.eval_region(session, module, region_id)?;
+        let term = match returned {
+            Some(Slot::Term(term)) => term,
+            Some(Slot::Boolean(value)) => session.builder().boolean(value, Default::default()),
+            Some(Slot::Unit) | None => session.builder().null(Default::default()),
+        };
+        let value = session.insert_symbolic_value(term);
+        let result = ComputationResult::with_status(ComputationStatus::Exact, CoverageStatus::Full)
+            .with_value(value)
+            .with_symbolic_term(term)
+            .with_provenance(ResultProvenance { request_kind: "ExecutionIR" });
+        Ok(session.insert_result(result))
+    }
+
+    fn eval_region(&self, session: &Session, module: &ExecutionModule, region_id: RegionId) -> Result<Option<Slot>> {
+        let region = module
+            .regions
+            .iter()
+            .find(|r| r.id == region_id)
+            .ok_or_else(|| diag("missing_region"))?;
+        let mut block_id = region.entry;
+        let mut slots: HashMap<SsaValueId, Slot> = HashMap::new();
+        // Bootstrap: linear / single-block regions and simple branches only.
+        for _ in 0..region.blocks.len().saturating_mul(4).max(4) {
+            let block = region
+                .blocks
+                .iter()
+                .find(|b| b.id == block_id)
+                .ok_or_else(|| diag("missing_block"))?;
+            for op in &block.operations {
+                let produced = self.eval_operation(session, module, &slots, &op.kind)?;
+                if let Some(result) = op.result {
+                    slots.insert(result, produced);
+                }
+            }
+            match &block.terminator {
+                Terminator::Return { values } => {
+                    if values.is_empty() {
+                        return Ok(Some(Slot::Unit));
+                    }
+                    let first = values[0];
+                    return Ok(Some(*slots.get(&first).ok_or_else(|| diag("return_undefined"))?));
+                }
+                Terminator::Branch { condition, then_edge, else_edge } => {
+                    let pred = match slots.get(condition).ok_or_else(|| diag("branch_undefined"))? {
+                        Slot::Boolean(v) => *v,
+                        _ => return Err(diag("branch_not_boolean")),
+                    };
+                    let edge = if pred { then_edge } else { else_edge };
+                    self.bind_edge_args(&mut slots, region, edge.target, &edge.arguments)?;
+                    block_id = edge.target;
+                }
+                Terminator::Reject { .. } => {
+                    return Err(Diagnostic::new(DiagnosticCode::UnsupportedOperation)
+                        .detail("component", "ReferenceExecutor")
+                        .detail("reason", "rejected"));
+                }
+                Terminator::Unreachable => return Err(diag("unreachable")),
+                Terminator::Switch { .. } | Terminator::Yield { .. } => {
+                    return Err(diag("terminator_not_implemented"));
+                }
+            }
+        }
+        Err(diag("block_visit_budget_exceeded"))
+    }
+
+    fn bind_edge_args(
+        &self,
+        slots: &mut HashMap<SsaValueId, Slot>,
+        region: &crate::execution::ir::Region,
+        target: BlockId,
+        arguments: &[SsaValueId],
+    ) -> Result<()> {
+        let block = region.blocks.iter().find(|b| b.id == target).ok_or_else(|| diag("edge_target_missing"))?;
+        if block.parameters.len() != arguments.len() {
+            return Err(diag("edge_arity_mismatch"));
+        }
+        for (param, arg) in block.parameters.iter().zip(arguments.iter()) {
+            let value = *slots.get(arg).ok_or_else(|| diag("edge_arg_undefined"))?;
+            slots.insert(param.value, value);
+        }
+        Ok(())
+    }
+
+    fn eval_operation(
+        &self,
+        _session: &Session,
+        module: &ExecutionModule,
+        slots: &HashMap<SsaValueId, Slot>,
+        kind: &OperationKind,
+    ) -> Result<Slot> {
+        match kind {
+            OperationKind::Constant { constant } => {
+                let value = module.constants.get(constant.0 as usize).ok_or_else(|| diag("missing_constant"))?;
+                Ok(match value {
+                    ConstantValue::Boolean(v) => Slot::Boolean(*v),
+                    ConstantValue::Term(term) => Slot::Term(*term),
+                    ConstantValue::Unit => Slot::Unit,
+                })
+            }
+            OperationKind::LoadTerm { root } => {
+                let captured = module.captured_roots.get(root.0 as usize).ok_or_else(|| diag("missing_root"))?;
+                match captured {
+                    CapturedRoot::Term(term) => Ok(Slot::Term(*term)),
+                    CapturedRoot::Value(_) | CapturedRoot::Result(_) => Err(diag("root_not_term")),
+                }
+            }
+            OperationKind::LoadInput { .. }
+            | OperationKind::ApplySemanticOperator { .. }
+            | OperationKind::ReadBinding { .. }
+            | OperationKind::WriteBinding { .. }
+            | OperationKind::EnterScope { .. }
+            | OperationKind::ExitScope { .. }
+            | OperationKind::CallProvider { .. }
+            | OperationKind::Guard { .. }
+            | OperationKind::MaterializeValue { .. }
+            | OperationKind::PublishResult { .. } => Err(diag("operation_not_implemented")),
+        }
+    }
+}
+
+fn diag(reason: &str) -> Diagnostic {
+    Diagnostic::new(DiagnosticCode::UnsupportedOperation)
+        .detail("component", "ReferenceExecutor")
+        .detail("reason", reason)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::api::request::AthenaRequest;
+    use crate::execution::compiler::ExecutionCompiler;
+    use crate::execution::ir::{
+        BasicBlock, BlockEdge, BlockId, ConstantId, ConstantValue, ExecutionValueType, ModuleFingerprint, Operation, Region,
+        RegionId, Terminator,
+    };
+
+    #[test]
+    fn execute_compiled_atom_term() {
+        let mut session = Session::new();
+        let term = session.builder().int(9, Default::default());
+        let module = ExecutionCompiler::new()
+            .compile(&session, &AthenaRequest::Term(term))
+            .expect("compile");
+        let result_id = ReferenceExecutor::new().execute(&mut session, &module).expect("execute");
+        let loaded = session.results.get(result_id).expect("result");
+        assert_eq!(loaded.symbolic_term, Some(term));
+        assert_eq!(loaded.status, ComputationStatus::Exact);
+        assert_eq!(loaded.coverage, CoverageStatus::Full);
+    }
+
+    #[test]
+    fn execute_boolean_branch() {
+        let cond = SsaValueId(0);
+        let then_v = SsaValueId(1);
+        let else_v = SsaValueId(2);
+        let entry = BasicBlock {
+            id: BlockId(0),
+            parameters: Vec::new(),
+            operations: vec![Operation {
+                result: Some(cond),
+                result_type: ExecutionValueType::Boolean,
+                kind: OperationKind::Constant {
+                    constant: ConstantId(0),
+                },
+                effect_in: None,
+                effect_out: None,
+            }],
+            terminator: Terminator::Branch {
+                condition: cond,
+                then_edge: BlockEdge::jump(BlockId(1)),
+                else_edge: BlockEdge::jump(BlockId(2)),
+            },
+        };
+        let then_block = BasicBlock {
+            id: BlockId(1),
+            parameters: Vec::new(),
+            operations: vec![Operation {
+                result: Some(then_v),
+                result_type: ExecutionValueType::Boolean,
+                kind: OperationKind::Constant {
+                    constant: ConstantId(1),
+                },
+                effect_in: None,
+                effect_out: None,
+            }],
+            terminator: Terminator::return_value(then_v),
+        };
+        let else_block = BasicBlock {
+            id: BlockId(2),
+            parameters: Vec::new(),
+            operations: vec![Operation {
+                result: Some(else_v),
+                result_type: ExecutionValueType::Boolean,
+                kind: OperationKind::Constant {
+                    constant: ConstantId(2),
+                },
+                effect_in: None,
+                effect_out: None,
+            }],
+            terminator: Terminator::return_value(else_v),
+        };
+        let region = Region {
+            id: RegionId(0),
+            entry: BlockId(0),
+            blocks: vec![entry, then_block, else_block],
+            result_types: vec![ExecutionValueType::Boolean],
+        };
+        let mut module = ExecutionModule {
+            inputs: Vec::new(),
+            constants: vec![ConstantValue::boolean(true), ConstantValue::boolean(true), ConstantValue::boolean(false)],
+            captured_roots: Vec::new(),
+            regions: vec![region],
+            effect_edges: Vec::new(),
+            exits: Vec::new(),
+            provider_calls: Vec::new(),
+            fingerprint: ModuleFingerprint(0),
+        };
+        module.fingerprint = ModuleFingerprint::of_module(&module);
+
+        let mut session = Session::new();
+        let result_id = ReferenceExecutor::new().execute(&mut session, &module).expect("branch");
+        let loaded = session.results.get(result_id).expect("result");
+        let term = loaded.symbolic_term.expect("term");
+        match session.arena.get(term) {
+            Some(athena_ir::TermNode::Atom(athena_ir::Atom::Boolean(true))) => {}
+            other => panic!("expected true boolean term, got {other:?}"),
+        }
     }
 }
