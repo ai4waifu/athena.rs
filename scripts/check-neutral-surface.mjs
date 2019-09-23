a2a839f30edeb4d869d0815ae8e8ce0a96859782
aster <aster@vers.site>
2019-09-23 08:28:45 +0000

    🔒 Tighten frontend-neutral surface source gate
    Scan more foundation crates. Ban Mathematica surface string literals and h.as_str match dispatch. Ignore doc-comment examples.

--- scripts/check-neutral-surface.mjs
+++ scripts/check-neutral-surface.mjs
@@ -10,8 +10,12 @@ import { fileURLToPath } from "node:url";
 const root = path.resolve(path.dirname(fileURLToPath(import.meta.url)), "..");
 
 const scanRoots = [
-  "projects/athena-engine/src",
+  "projects/athena-types/src",
+  "projects/athena-gc/src",
+  "projects/athena-numeric/src",
   "projects/athena-ir/src",
+  "projects/athena-rewriter/src",
+  "projects/athena-engine/src",
   "projects/athena/src",
   "projects/athena-testing/src",
 ];
@@ -20,16 +24,52 @@ const scanRoots = [
 const banned = [
   { re: /\btry_calculus_request\b/, why: "string calculus hub" },
   { re: /\blookup_function\s*\(/, why: "string function registry" },
-  { re: /push_application\(\s*[^,]+,\s*"(Plus|Times|Simplify|Integrate|Limit|Define|LinearSolve|Cond|Recover|Sin|Cos)"/, why: "named-head core construction" },
-  { re: /push_application_named\(\s*[^,]+,\s*"(Plus|Times|Simplify|Integrate|Limit|Define|LinearSolve|Cond|Recover|Sin|Cos)"/, why: "named-head core construction" },
-  { re: /operators\.intern\(\s*"(Plus|Times|Simplify|Integrate|Limit|Define|LinearSolve|Cond|Recover|Sin|Cos)"\s*\)/, why: "Extension intern of core surface name" },
-  { re: /match\s+name\.as_str\(\)\s*\{[\s\S]{0,200}"(LinearSolve|Import|Export|Timing|Define|CountedLoop|Cond|Recover)"/, why: "Extension surface-name match dispatch" },
+  { re: /\bhead_name\s*[:=(]/, why: "string head semantic hub" },
+  {
+    re: /push_application(?:_named)?\(\s*[^,]+,\s*"(Plus|Times|Simplify|Integrate|Limit|Define|LinearSolve|Cond|Recover|Sin|Cos|D|SetDelayed|RuleDelayed|OwnValue|DownValue|Blank)"/,
+    why: "named-head core construction",
+  },
+  {
+    re: /operators\.intern\(\s*"(Plus|Times|Simplify|Integrate|Limit|Define|LinearSolve|Cond|Recover|Sin|Cos|D|SetDelayed|RuleDelayed|OwnValue|DownValue)"\s*\)/,
+    why: "Extension intern of core surface name",
+  },
+  {
+    re: /match\s+name\.as_str\(\)\s*\{[\s\S]{0,240}"(LinearSolve|Import|Export|Timing|Define|CountedLoop|Cond|Recover|Plus|Times|Integrate|Limit|D)"/,
+    why: "Extension surface-name match dispatch",
+  },
+  {
+    re: /match\s+h\.as_str\(\)\s*\{/,
+    why: "string head match dispatch",
+  },
+  // Mathematica surface names as Rust string literals (not neutral debug_label Add/Multiply).
+  {
+    re: /"(Plus|Times|SetDelayed|RuleDelayed|OwnValue|DownValue|HoldAll|BlankSequence|BlankNullSequence|WriteDownValue)"/,
+    why: "Mathematica surface string literal in core",
+  },
 ];
 
+/** Paths allowed to mention surface strings (display / docs / fixtures). */
 const allowPath = (rel) => {
-  // Unit tests under src may keep Extension residual fixtures.
   if (rel.includes("/tests/") || rel.endsWith("/tests.rs")) return true;
   if (rel.includes("\\tests\\") || rel.endsWith("\\tests.rs")) return true;
+  // Neutral debug_label may still spell Integrate / Limit (operator names, not Plus).
+  if (rel.endsWith("/operator/semantic.rs")) return false;
+  // Boundary negative tests live under src sometimes.
+  if (rel.includes("/boundary/")) return true;
+  return false;
+};
+
+/** Per-file allow for specific ban reasons (display helpers only). */
+const allowMatch = (rel, why) => {
+  if (why === "Mathematica surface string literal in core") {
+    // No core file should carry Plus/Times literals today.
+    return false;
+  }
+  if (why === "string head semantic hub") {
+    // `application_display_name` / comments mentioning head_name history are ok if we only ban `head_name` token as API.
+    // Keep strict: only allow diagnostics comments that don't define the API.
+    if (rel.includes("/diagnostics/")) return true;
+  }
   return false;
 };
 
@@ -43,9 +83,21 @@ for (const relRoot of scanRoots) {
     const rel = path.relative(root, file).replace(/\\/g, "/");
     if (allowPath(rel)) return;
     const text = fs.readFileSync(file, "utf8");
+    // Strip line comments so doc examples like `head_name: String` do not trip the gate.
+    const code = text
+      .split("\n")
+      .map((line) => {
+        const t = line.trimStart();
+        if (t.startsWith("//") || t.startsWith("*") || t.startsWith("//!") || t.startsWith("///")) {
+          return "";
+        }
+        return line;
+      })
+      .join("\n");
     for (const { re, why } of banned) {
-      const m = text.match(re);
+      const m = code.match(re);
       if (m) {
+        if (allowMatch(rel, why)) continue;
         failures.push(`${rel}: ${why} (matched ${JSON.stringify(m[0].slice(0, 80))})`);
       }
     }
