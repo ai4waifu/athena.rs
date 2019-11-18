5320bc437c671daa25cee8c75540a13fc596c14e
aster <aster@vers.site>
2019-11-18 20:50:06 +0000

    ✨ Store `RuleDispatch` tables under `DispatchTableId`
    `DefinitionLayer` allocates `DispatchTableId` per extension operator.
    Apply still indexes via `OperatorId` to table. Owners clear whole tables.

--- projects/athena-engine/src/execution/environment/definitions.rs
+++ projects/athena-engine/src/execution/environment/definitions.rs
@@ -1,12 +1,12 @@
-//! 分层定义表与作用域帧（Living `25` / `27` · `SymbolId` 绑定 · `OperatorId` 规则）。
+//! 分层定义表与作用域帧（Living `25` / `27` · `SymbolId` 绑定 · `DispatchTableId` 规则）。
 //!
 //! - [`DefinitionLayer`]：语句层立即绑定 / 残余绑定 / 规则分派（Session 全局为底层）。
 //! - [`ScopeFrame`]：局部遮蔽（读取优先，不写回全局）。
-//! - 规则分派只按 [`OperatorId`] 索引；清规则经 `SymbolId`→`OperatorId` 拥有映射（禁止 `operators.lookup` 字符串桥）。
+//! - 规则表按 [`DispatchTableId`] 存储；[`OperatorId`] 仅作 Extension apply 索引（Living `27`）。
 
 use std::collections::HashMap;
 
-use athena_types::{OperatorId, SymbolId, TermId};
+use athena_types::{DispatchTableId, OperatorId, SymbolId, TermId};
 
 use crate::reasoning::trs::TermPattern;
 
@@ -15,10 +15,13 @@ use crate::reasoning::trs::TermPattern;
 pub struct DefinitionLayer {
     bindings: HashMap<SymbolId, TermId>,
     residual_bindings: HashMap<SymbolId, TermId>,
-    /// Extension / 表面 head 规则（Living `27`）。
-    extension_dispatch_rules: HashMap<OperatorId, Vec<(TermPattern, TermId)>>,
-    /// `RegisterRuleDispatch` 头符号对其规则表的拥有关系（清绑定无需字符串 lookup）。
+    /// 规则分派表（Living `27` 一等键）。
+    dispatch_tables: HashMap<DispatchTableId, Vec<(TermPattern, TermId)>>,
+    /// Extension head → 其分派表（apply 路径索引，非字符串）。
+    operator_tables: HashMap<OperatorId, DispatchTableId>,
+    /// `RegisterRuleDispatch` 头符号对其 Extension operator 的拥有关系。
     extension_rule_owners: HashMap<SymbolId, OperatorId>,
+    next_dispatch_table: u32,
 }
 
 impl DefinitionLayer {
@@ -41,12 +44,13 @@ impl DefinitionLayer {
         self.clear_owned_extension(symbol);
     }
 
-    /// 追加 extension head 规则（[`OperatorId`] 键）。无符号拥有关系时由调用方自管生命周期。
+    /// 追加 extension head 规则（分配或复用 [`DispatchTableId`]）。
     pub fn register_extension_rule(&mut self, op: OperatorId, pattern: TermPattern, replacement: TermId) {
-        self.extension_dispatch_rules.entry(op).or_default().push((pattern, replacement));
+        let table = self.ensure_table_for_operator(op);
+        self.dispatch_tables.entry(table).or_default().push((pattern, replacement));
     }
 
-    /// 以用户符号拥有 extension 规则表（清除该符号值绑定，记录 `SymbolId`→`OperatorId`）。
+    /// 以用户符号拥有 extension 规则表（清除该符号值绑定，记录拥有关系）。
     pub fn register_extension_rule_for_symbol(
         &mut self,
         symbol: SymbolId,
@@ -70,9 +74,15 @@ impl DefinitionLayer {
         self.residual_bindings.get(&symbol).copied()
     }
 
-    /// 查 extension head 规则分派表。
+    /// Extension apply：经 `OperatorId` → [`DispatchTableId`] 取规则。
     pub fn extension_dispatch_rules(&self, op: OperatorId) -> Option<&[(TermPattern, TermId)]> {
-        self.extension_dispatch_rules.get(&op).map(Vec::as_slice)
+        let table = self.operator_tables.get(&op)?;
+        self.dispatch_tables.get(table).map(Vec::as_slice)
+    }
+
+    /// 查 Extension head 对应的分派表句柄。
+    pub fn dispatch_table_for(&self, op: OperatorId) -> Option<DispatchTableId> {
+        self.operator_tables.get(&op).copied()
     }
 
     /// 清除该符号的绑定及其拥有的 extension 规则。
@@ -82,9 +92,11 @@ impl DefinitionLayer {
         self.clear_owned_extension(symbol);
     }
 
-    /// 清除 extension head 的分派规则（并拆除反向拥有映射）。
+    /// 清除 Extension head 对应的分派表。
     pub fn clear_extension(&mut self, op: OperatorId) {
-        self.extension_dispatch_rules.remove(&op);
+        if let Some(table) = self.operator_tables.remove(&op) {
+            self.dispatch_tables.remove(&table);
+        }
         self.extension_rule_owners.retain(|_, owned| *owned != op);
     }
 
@@ -92,8 +104,10 @@ impl DefinitionLayer {
     pub fn clear(&mut self) {
         self.bindings.clear();
         self.residual_bindings.clear();
-        self.extension_dispatch_rules.clear();
+        self.dispatch_tables.clear();
+        self.operator_tables.clear();
         self.extension_rule_owners.clear();
+        self.next_dispatch_table = 0;
     }
 
     /// 层内是否存在该符号的任何绑定或规则拥有关系。
@@ -103,9 +117,19 @@ impl DefinitionLayer {
             || self.extension_rule_owners.contains_key(&symbol)
     }
 
+    fn ensure_table_for_operator(&mut self, op: OperatorId) -> DispatchTableId {
+        if let Some(id) = self.operator_tables.get(&op).copied() {
+            return id;
+        }
+        let id = DispatchTableId(self.next_dispatch_table);
+        self.next_dispatch_table = self.next_dispatch_table.saturating_add(1);
+        self.operator_tables.insert(op, id);
+        id
+    }
+
     fn clear_owned_extension(&mut self, symbol: SymbolId) {
         if let Some(op) = self.extension_rule_owners.remove(&symbol) {
-            self.extension_dispatch_rules.remove(&op);
+            self.clear_extension(op);
         }
     }
 }
