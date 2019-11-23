5f9cc363c171d9ac0fcbcb3b45703d7bf2afff70
aster <aster@vers.site>
2019-11-23 18:21:33 +0000

    ✨ Scaffold M-Graph equality `ProofForest`
    Append-only justification edges for admitted equalities.
    Distinct from E-Graph candidate unions and `ExactUnionFind`.

--- projects/athena-engine/src/reasoning/mgraph/equivalence/mod.rs
+++ projects/athena-engine/src/reasoning/mgraph/equivalence/mod.rs
@@ -1,7 +1,9 @@
 //! Exact union-find 与同余。
 
 pub mod congruence;
+pub mod proof_forest;
 pub mod union_find;
 
 pub use congruence::*;
+pub use proof_forest::{ProofEdge, ProofForest, ProofStepKind};
 pub use union_find::ExactUnionFind;
---
+++ projects/athena-engine/src/reasoning/mgraph/equivalence/proof_forest.rs
@@ -0,0 +1,80 @@
+//! Proof forest for admitted equalities (Living `03` R-2.4 · bootstrap).
+//!
+//! Records *why* two terms are equal after AdmissionGate. Distinct from
+//! scope-local E-Graph candidate unions and from operational [`ExactUnionFind`].
+
+use athena_types::TermId;
+
+/// One justified equality edge in the forest.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ProofEdge {
+    /// Left term.
+    pub left: TermId,
+    /// Right term.
+    pub right: TermId,
+    /// Opaque step kind (filled by verifiers later).
+    pub step_kind: ProofStepKind,
+}
+
+/// Closed step taxonomy for bootstrap (expand with certificates later).
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ProofStepKind {
+    /// Direct admitted equality.
+    AdmittedEquality,
+    /// Congruence under a common head.
+    Congruence,
+    /// Transitivity step.
+    Transitivity,
+}
+
+/// Forest of equality justifications (append-only bootstrap).
+#[derive(Debug, Default, Clone)]
+pub struct ProofForest {
+    edges: Vec<ProofEdge>,
+}
+
+impl ProofForest {
+    /// Empty forest.
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Record a justified equality (does not itself admit M-Graph facts).
+    pub fn record(&mut self, left: TermId, right: TermId, step_kind: ProofStepKind) {
+        self.edges.push(ProofEdge {
+            left,
+            right,
+            step_kind,
+        });
+    }
+
+    /// Edge count.
+    pub fn len(&self) -> usize {
+        self.edges.len()
+    }
+
+    /// Whether empty.
+    pub fn is_empty(&self) -> bool {
+        self.edges.is_empty()
+    }
+
+    /// Iterate edges.
+    pub fn edges(&self) -> &[ProofEdge] {
+        &self.edges
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use athena_types::TermId;
+
+    use super::*;
+
+    #[test]
+    fn proof_forest_records_admitted_equality_edges() {
+        let mut forest = ProofForest::new();
+        forest.record(TermId(1), TermId(2), ProofStepKind::AdmittedEquality);
+        assert_eq!(forest.len(), 1);
+        assert_eq!(forest.edges()[0].step_kind, ProofStepKind::AdmittedEquality);
+    }
+}
