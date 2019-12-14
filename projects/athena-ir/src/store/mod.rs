74943ecc4d9d8c08147ea8fd743f71acae6cb8ab
aster <aster@vers.site>
2019-12-14 12:29:49 +0000

    ✨ Hash-cons `TermStore::push` by structural `ConsKey`
    Equal atoms applications and collections share TermId. Numbers key on render plus fingerprint_domain_tag so NumericValue need not implement Hash.

--- projects/athena-ir/src/store/mod.rs
+++ projects/athena-ir/src/store/mod.rs
@@ -1,8 +1,59 @@
 //! `TermStore` — Core IR 唯一符号项存储。
 
-use athena_types::{Diagnostic, DiagnosticCode, Result, SourceSpan, TermId};
+use std::collections::HashMap;
 
-use crate::{node::TermNode, symbol::SymbolTable};
+use athena_types::{CollectionKind, Diagnostic, DiagnosticCode, Result, SourceSpan, SymbolId, TermId};
+
+use crate::{
+    node::{Atom, MathematicalConstant, TermNode},
+    operator::ApplicationHead,
+    symbol::SymbolTable,
+};
+
+/// Structural key for hash-consing (Living `26` · stable identity, not arena index).
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum ConsKey {
+    Symbol(SymbolId),
+    Boolean(bool),
+    Null,
+    Constant(MathematicalConstant),
+    String(String),
+    /// Number identity via render + domain tag (avoids requiring `Hash` on `NumericValue`).
+    Number {
+        render: String,
+        domain_tag: u64,
+    },
+    Collection {
+        kind: CollectionKind,
+        elements: Vec<TermId>,
+    },
+    Application {
+        head: ApplicationHead,
+        arguments: Vec<TermId>,
+    },
+}
+
+fn cons_key(kind: &TermNode) -> ConsKey {
+    match kind {
+        TermNode::Atom(Atom::Symbol(id)) => ConsKey::Symbol(*id),
+        TermNode::Atom(Atom::Boolean(b)) => ConsKey::Boolean(*b),
+        TermNode::Atom(Atom::Null) => ConsKey::Null,
+        TermNode::Atom(Atom::Constant(c)) => ConsKey::Constant(*c),
+        TermNode::Atom(Atom::String(s)) => ConsKey::String(s.clone()),
+        TermNode::Atom(Atom::Number(n)) => ConsKey::Number {
+            render: n.to_render_string(),
+            domain_tag: n.fingerprint_domain_tag(),
+        },
+        TermNode::Collection { kind, elements } => ConsKey::Collection {
+            kind: *kind,
+            elements: elements.clone(),
+        },
+        TermNode::Application { head, arguments } => ConsKey::Application {
+            head: *head,
+            arguments: arguments.clone(),
+        },
+    }
+}
 
 /// Core CAS IR 符号项存储。
 #[derive(Debug, Default)]
@@ -10,6 +61,7 @@ pub struct TermStore {
     nodes: Vec<TermNode>,
     spans: Vec<SourceSpan>,
     symbols: SymbolTable,
+    cons: HashMap<ConsKey, TermId>,
 }
 
 impl TermStore {
@@ -28,11 +80,18 @@ impl TermStore {
         &mut self.symbols
     }
 
-    /// 分配 term 节点，返回稳定 [`TermId`]。
+    /// 分配或复用 term 节点（hash-cons），返回稳定 [`TermId`]。
+    ///
+    /// Equal structure under [`ConsKey`] shares one id. First-seen span is kept.
     pub fn push(&mut self, kind: TermNode, span: SourceSpan) -> TermId {
+        let key = cons_key(&kind);
+        if let Some(id) = self.cons.get(&key) {
+            return *id;
+        }
         let id = TermId(self.nodes.len() as u32);
         self.nodes.push(kind);
         self.spans.push(span);
+        self.cons.insert(key, id);
         id
     }
 
@@ -99,14 +158,66 @@ fn verify_term(arena: &TermStore, id: TermId, stack: &mut Vec<TermId>) -> Result
         return Err(Diagnostic::new(DiagnosticCode::InvalidIndex));
     };
     stack.push(id);
-    match kind {
-        TermNode::Atom(_) => {}
-        TermNode::Collection { elements: items, .. } | TermNode::Application { arguments: items, .. } => {
-            for child in items {
+    let result = match kind {
+        TermNode::Atom(_) => Ok(()),
+        TermNode::Collection { elements, .. } => {
+            for child in elements {
                 verify_term(arena, *child, stack)?;
             }
+            Ok(())
         }
-    }
+        TermNode::Application { arguments, .. } => {
+            for child in arguments {
+                verify_term(arena, *child, stack)?;
+            }
+            Ok(())
+        }
+    };
     stack.pop();
-    Ok(())
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use athena_types::SourceSpan;
+
+    use super::*;
+    use crate::{ApplicationHead, SemanticOperator};
+
+    #[test]
+    fn push_hash_conses_equal_atoms_and_applications() {
+        let mut store = TermStore::new();
+        let span = SourceSpan::default();
+        let a = store.push(TermNode::Atom(Atom::Boolean(true)), span);
+        let b = store.push(TermNode::Atom(Atom::Boolean(true)), span);
+        assert_eq!(a, b);
+        assert_eq!(store.len(), 1);
+
+        let one = store.push(
+            TermNode::Atom(Atom::Number(athena_numeric::Number::small_int(1))),
+            span,
+        );
+        let one_again = store.push(
+            TermNode::Atom(Atom::Number(athena_numeric::Number::small_int(1))),
+            span,
+        );
+        assert_eq!(one, one_again);
+
+        let add1 = store.push(
+            TermNode::Application {
+                head: ApplicationHead::Semantic(SemanticOperator::Add),
+                arguments: vec![one, one],
+            },
+            span,
+        );
+        let add2 = store.push(
+            TermNode::Application {
+                head: ApplicationHead::Semantic(SemanticOperator::Add),
+                arguments: vec![one, one],
+            },
+            span,
+        );
+        assert_eq!(add1, add2);
+        assert_eq!(store.len(), 3);
+    }
 }
