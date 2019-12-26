ff052bdae17fc4b65cf138c7e758ef780e4dbb6e
aster <aster@vers.site>
2019-12-26 16:17:57 +0000

    🧪 Cover `SmallestAst` E-Graph extract in `athena-testing`
    After typed Add(x,x) saturation the extractor prefers the bound integer over the application.

--- projects/athena-testing/src/contract.rs
+++ projects/athena-testing/src/contract.rs
@@ -377,3 +377,42 @@ fn typed_egraph_saturation_binds_add_same() {
     assert_eq!(report.candidates[0].right_term, one);
     assert_eq!(fx.session().mgraph.semantic.derived.proof_forest.len(), 0);
 }
+
+#[test]
+fn egraph_extract_smallest_ast_after_typed_saturation() {
+    use athena_engine::reasoning::egraph::{ExtractionPreference, TypedRuleSet};
+    use athena_engine::reasoning::trs::TermPattern;
+    use athena_ir::{ApplicationHead, SemanticOperator};
+
+    let mut fx = SessionFixture::new();
+    let (one, add, x_sym, x_term) = {
+        let mut t = fx.terms();
+        let one = t.integer(1);
+        let add = t.add([one, one]);
+        let x_sym = t.intern("x");
+        let x_term = t.symbol("x");
+        (one, add, x_sym, x_term)
+    };
+    let pattern = TermPattern::Application {
+        operator: ApplicationHead::Semantic(SemanticOperator::Add),
+        arguments: vec![
+            TermPattern::Bind {
+                name: x_sym,
+                inner: Box::new(TermPattern::Any),
+            },
+            TermPattern::Bind {
+                name: x_sym,
+                inner: Box::new(TermPattern::Any),
+            },
+        ],
+    };
+    let mut rules = TypedRuleSet::new();
+    rules.push(pattern, x_term, Some("add_same"));
+    let _ = fx.session_mut().run_egraph_saturation_typed(&[add], Some(&rules));
+    let class = fx.session().egraph.class_of_term(add).expect("class");
+    let extracted = fx
+        .session()
+        .extract_egraph_class(class, ExtractionPreference::SmallestAst)
+        .expect("extract");
+    assert_eq!(extracted, one);
+}
